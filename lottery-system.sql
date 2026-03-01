df0c6835bc4d7ccb48c566d56ee907ebdedf9b2c
Asawinhinlekfire <deer25480215@gmail.com>
2026-03-01 19:27:37 +0700

    Add files via upload

--- lottery-system.sql
+++ lottery-system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2026 at 08:10 AM
+-- Generation Time: Mar 01, 2026 at 01:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `lottery-system`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buylottery`
+--
+
+CREATE TABLE `buylottery` (
+  `lotteryID` varchar(8) NOT NULL,
+  `numLottery` int(6) DEFAULT NULL,
+  `unitLottery` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `buylottery`
+--
+
+INSERT INTO `buylottery` (`lotteryID`, `numLottery`, `unitLottery`) VALUES
+('b000000', 545434, 1),
+('b000001', 123132, 18),
+('b000002', 132321, 18);
 
 -- --------------------------------------------------------
 
@@ -67,6 +88,20 @@ INSERT INTO `draws` (`draw_id`, `draw_date`, `draw_name`, `status`, `created_at`
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `login_log`
+--
+
+CREATE TABLE `login_log` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `login_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `lotteries`
 --
 
@@ -108,6 +143,46 @@ INSERT INTO `lotteries` (`lottery_id`, `lotteryNumber`, `price`, `draw_id`, `sta
 ('4321657800', 321654, 120, '33333333', 'sold', '2026-02-19'),
 ('8765432100', 222222, 120, '22222222', 'reserved', '2026-01-19');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset`
+--
+
+CREATE TABLE `password_reset` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `reset_token` varchar(255) NOT NULL,
+  `expire_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(100) DEFAULT NULL,
+  `lastname` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `role` enum('user','admin') DEFAULT 'user',
+  `status` tinyint(1) DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `firstname`, `lastname`, `phone`, `role`, `status`, `created_at`, `updated_at`) VALUES
+(13, 'Abyss', 'abcd1234@email.com', '$2y$10$Z4gxgyasY8MVSfmEQrgQUe09xGEglZ/vo2Ng2ym0g.YxMtNx.bm3a', 'Asawin', 'Khachinrot', '0000000000', 'user', 1, '2026-03-01 11:40:19', '2026-03-01 12:04:31');
+
 --
 -- Indexes for dumped tables
 --
@@ -119,11 +194,53 @@ ALTER TABLE `draws`
   ADD PRIMARY KEY (`draw_id`);
 
 --
+-- Indexes for table `login_log`
+--
+ALTER TABLE `login_log`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `lotteries`
 --
 ALTER TABLE `lotteries`
   ADD PRIMARY KEY (`lottery_id`),
   ADD KEY `draw_id` (`draw_id`);
+
+--
+-- Indexes for table `password_reset`
+--
+ALTER TABLE `password_reset`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login_log`
+--
+ALTER TABLE `login_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `password_reset`
+--
+ALTER TABLE `password_reset`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
